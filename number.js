8b5c6bb653b012d51bd970ec2ff9afc9e893ec93
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-02-28 02:01:08 +0600

    parsefloat / pardeInt and to fixed use

---
+++ number.js
@@ -0,0 +1,15 @@
+var applePrice = 200;
+var dragonPrice = parseFloat('30.5');
+// var dragonPrice = parseInt('30.5');
+
+// console.log(dragonPrice);
+// console.log(applePrice);
+console.log(dragonPrice +applePrice);
+
+
+
+
+var frist = 0.2;
+var secend = 0.1;
+var total = frist + secend;
+console.log(total.toFixed(1));
