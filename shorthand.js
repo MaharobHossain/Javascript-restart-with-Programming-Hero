ead6dfa274fc6e8c46676a58513cb36759cc20d7
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-02-28 12:31:44 +0600

    Short hand use

---
+++ shorthand.js
@@ -0,0 +1,12 @@
+// Short hand write
+
+
+var eggPrice = 10;
+eggPrice *= 5;
+// totalEggPrice = eggPrice * 3;
+// console.log(eggPrice);
+
+
+frigePrice = 40000;
+frigePrice -=2000;
+console.log(frigePrice);
