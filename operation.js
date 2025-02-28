151275866bbabb1add196ff4cec9fac7b4d46b20
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-02-28 12:11:10 +0600

    Price mantained

--- operation.js
+++ operation.js
@@ -2,10 +2,23 @@ var eggPrice = 40;
 var aluPrice = 20;
 
 // var total = eggPrice - aluPrice;
-console.log(aluPrice + eggPrice);
-console.log(aluPrice - eggPrice);
-console.log(aluPrice * eggPrice);
-console.log(aluPrice / eggPrice);
+// console.log(aluPrice + eggPrice);
+// console.log(aluPrice - eggPrice);
+// console.log(aluPrice * eggPrice);
+// console.log(aluPrice / eggPrice);
 
-console.log(10/3);
-console.log(18%5); // Remainder ba Vag-sesh
+// console.log(10/3);
+// console.log(18%5); // Remainder ba Vag-sesh
+
+
+
+
+
+
+
+var oldPrice = 30;
+ presentPrice = oldPrice + 15;
+  updatedPrice = presentPrice + 55;
+  discountPrice = updatedPrice - 10;
+
+  console.log(discountPrice);
