b66cfafefe9517f1d029b7707bca655f5c32881f
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-02-28 12:00:47 +0600

    Operation jss and reminder

---
+++ operation.js
@@ -0,0 +1,11 @@
+var eggPrice = 40;
+var aluPrice = 20;
+
+// var total = eggPrice - aluPrice;
+console.log(aluPrice + eggPrice);
+console.log(aluPrice - eggPrice);
+console.log(aluPrice * eggPrice);
+console.log(aluPrice / eggPrice);
+
+console.log(10/3);
+console.log(18%5); // Remainder ba Vag-sesh
