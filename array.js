49ef3c3a96ae7981008718f1b4a09497937f4291
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-02 02:18:49 +0600

    array use

---
+++ array.js
@@ -0,0 +1,15 @@
+ 
+//  Array length use
+ const numbers = [1, 3, 7, 34, 23, 87, 84, 34, 23, 87, 84, 98, 87, 84, 34, 23, 87, 84, 98];
+//  console.log(numbers.length);
+
+
+
+
+const age = [];
+const number = [ 84, 98, 87, 84];
+
+console.log(number);
+number.push(102);
+number.push(102, 57676);
+console.log(number);
