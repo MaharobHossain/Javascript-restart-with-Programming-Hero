f370eb4c8ffedd58da55ced083ac4c01ac1d866d
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-01 13:31:59 +0600

    nested or multiple if else use in one segment

---
+++ nested.js
@@ -0,0 +1,22 @@
+const money = 0;
+
+if (money >=300){
+    console.log("Bro, you are Rich !!!");
+}
+
+else{
+    if(money >100){
+        console.log("Mota Muti chole tor sathe !!!");
+    }
+    else{
+        if( money > 50){
+            console.log("Kire mama , taka koi re !!!")
+        }
+        else if(money >0){
+            console.log("Halai akta fokinni !!!")
+        }
+        else{
+            console.log("Tui amr bondhu nah,halai !!!")
+        }
+    }
+}
