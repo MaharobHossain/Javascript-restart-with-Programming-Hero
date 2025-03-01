42558d047c5b9dc08564d0da41c69ba0defd006e
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-02 02:28:10 +0600

    Array push / pop use

---
+++ pushpop.js
@@ -0,0 +1,12 @@
+// Use of Push Pop
+
+const names = [ "Alif", "Maharob", "Opi"];
+// names.push("Hossain");
+names.pop();
+console.log(names);
+
+
+const numbers = [ 12, 34, 56, 76];
+numbers.push( 100);
+// numbers.pop();
+console.log(numbers);
