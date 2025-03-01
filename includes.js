f1fc10b9b7c27b71fd5e8f44874b395f9bc379a4
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-02 02:56:03 +0600

    Array includes use

---
+++ includes.js
@@ -0,0 +1,11 @@
+const friends = ["Kasem", "Daimul", "bati", "kiminal", "Alaa"]
+console.log(friends.includes("Daimul"));
+console.log(friends.includes("Opi"));
+
+
+if (friends.includes("saimul")){
+    console.log("Chol mama party kori");
+}
+else{
+    console.log("H**a mara de mama");
+}
