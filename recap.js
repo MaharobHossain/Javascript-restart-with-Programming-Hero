86509bc5532b69456b25cc632102530276e8d0e7
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-02 12:30:22 +0600

    Every topic recap is here or notes topic for revetions

---
+++ recap.js
@@ -0,0 +1,17 @@
+      
+                     //    Javascript Introduction Topic----->
+
+// Variable use----->
+const number = 25;
+
+// Condition use----->
+if (number> 20){
+
+}
+else{
+
+}
+
+// Array use------>
+const amount = [12, 34, 56, 67, 89];
+const name = ["salam", "rofiq", "jabbar", "borkot"];
