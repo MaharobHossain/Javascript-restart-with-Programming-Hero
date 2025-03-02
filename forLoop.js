e799fed101d2868dddc3cb964bb9fbbfb87338bb
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-02 13:24:46 +0600

    for  loop use in js

---
+++ forLoop.js
@@ -0,0 +1,15 @@
+const numbers = [12, 34, 56, 67, 78, 68];
+console.log(numbers);
+
+for( const item of numbers){
+    console.log(item + " aita num");
+}
+
+
+const fruits = ["Amra", "Lichu", "Kathal", "Kola"];
+console.log(fruits);
+
+for ( const item of fruits ) {
+    console.log(item + " Khabo ami");
+    console.log("Kawre khaite dibo nah,ami akai khabo");
+}
