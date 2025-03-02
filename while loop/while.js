943221c773e5280869dff7c9d4961f81929d232b
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-02 17:05:42 +0600

    While loop use primary

---
+++ while loop/while.js
@@ -0,0 +1,12 @@
+
+        //   ___________While Lopp Use___________
+
+        const num = 0;
+        
+        while (num < 5){
+            console.log("Mama dhor khaice");
+            num = num +1;
+            num += 1;
+            num++;
+        }
+        
