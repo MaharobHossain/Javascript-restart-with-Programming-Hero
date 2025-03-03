8722d8950b7727dbdbc287ed1f5c7d60a88824a9
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-03 06:58:24 +0600

    While loop use in js

---
+++ whileLoop.js
@@ -0,0 +1,25 @@
+
+        //   ___________While Lopp Use___________
+
+        // const num = 0;
+        
+        // while (num < 5){
+        //     console.log("Mama dhora khaice");
+        // //     num = num +1;
+        // //     num += 1;
+        //     num++;
+        // }
+
+
+
+
+
+
+
+
+
+let num = 50;
+while (num <= 100){
+   console.log(num);
+   num++;
+}
