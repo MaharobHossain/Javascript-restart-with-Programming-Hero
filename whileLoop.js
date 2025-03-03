aee5bdf617ded672488aa12e999fed3d7375ff16
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-03 12:41:46 +0600

    While loop task one complete

--- whileLoop.js
+++ whileLoop.js
@@ -21,15 +21,22 @@
 //    num++;
 // }
 
-    
+
+ //   ___________While Lopp Task one___________
+
+      let num = 1;
+      while ( num <= 50){
+       console.log("Number :", num, "I will invest at least 6 hrs every single day for next 60 days!");
+       num++;
+      }
 
 
 
-     let num = 1;
-     let sum = 0;
-     while(num <= 10){
-        console.log(num);
-        sum = sum + num;
-        console.log('sum', sum);
-        num++;
-     }
+//      let num = 1;
+//      let sum = 0;
+//      while(num <= 10){
+//         console.log(num);
+//         sum = sum + num;
+//         console.log('sum', sum);
+//         num++;
+//      }
