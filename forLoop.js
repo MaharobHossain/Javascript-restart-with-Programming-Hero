e09915b0d9e14f6871de554f9e401f531ee8e256
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-03 14:09:04 +0600

    For of loop and normal for loop and even/odd

--- forLoop.js
+++ forLoop.js
@@ -1,3 +1,5 @@
+//___________For Of Loop___________
+
 const numbers = [12, 34, 56, 67, 78, 68];
 console.log(numbers);
 
@@ -13,3 +15,41 @@ for ( const item of fruits ) {
     console.log(item + " Khabo ami");
     console.log("Kawre khaite dibo nah,ami akai khabo");
 }
+
+
+
+
+
+
+
+//________Normal for loop_______
+
+/**
+ *     for(    ;     :     ) {
+ *     }
+ * 
+ *  *     for(  1st part  ;  2nd part   :  3rd part   ) {
+ *     }
+ */
+
+
+  for (let num = 2 ; num < 7 ; num ++){
+    console.log(num);
+  }
+
+
+ 
+  
+  //________For loop even (+)_______
+
+  for (let number = 2 ; number <= 19 ; number = number + 2){
+    console.log(number);
+  }
+
+
+
+    //________For loop odd (-)_______
+
+    for (let numbers = 1 ; numbers <= 19 ; numbers = numbers + 2){
+        console.log(numbers);
+      }
