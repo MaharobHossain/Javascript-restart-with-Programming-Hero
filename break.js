aa7262b92569bf7fcab915a03dbcf93a90e61ec5
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-05 03:07:06 +0600

    Use of break in while loop js

--- break.js
+++ break.js
@@ -1,4 +1,4 @@
-// __________Use of break in js___________
+// __________Use of break in for loop js___________
 
 for( i = 0 ; i < 150 ; i++){
   console.log(i);
@@ -7,3 +7,17 @@ for( i = 0 ; i < 150 ; i++){
   }
 }
 console.log("Life end");
+
+
+
+// __________Use of break in while loop js___________
+
+let n = 54;
+
+while( n > 25 ){
+    if(n <= 50){
+        break;
+    }
+console.log(n);
+n--;
+}
