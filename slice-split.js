30664d3dcd1a34f158b38002e3f0bc1d66f051dd
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-06 20:49:45 +0600

    Joint and split use in js

--- slice-split.js
+++ slice-split.js
@@ -10,5 +10,16 @@ console.log(part);
 //_________Split use in js_________
 
 const nam = "Maharob Hossain";
-// console.log(nam.split(' '));
+console.log(nam.split(' '));
 console.log(nam.split("a"));
+
+const friendsStr = "Kiminal,kasem,alaa,bati,daimul";
+const friends = friendsStr.split(",");
+console.log(friends);
+
+//_________joint use in js_________
+
+const realFriend = [ 'Kiminal', 'kasem', 'alaa', 'bati', 'daimul' ];
+console.log(realFriend.join());
+console.log(realFriend.join(' ! '));
+console.log(realFriend.join('-'));
