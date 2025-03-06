e9dd07395e16ac839178de2289e7eb86a3814b49
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-06 20:33:21 +0600

    Split use in js

--- slice.js
+++ slice.js
@@ -3,3 +3,12 @@
 const school = "Bahadurpur High School ";
 const part = school.slice(16,23);
 console.log(part);
+
+
+
+
+//_________Split use in js_________
+
+const nam = "Maharob Hossain";
+// console.log(nam.split(' '));
+console.log(nam.split("a"));
