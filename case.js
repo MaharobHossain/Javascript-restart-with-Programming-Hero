ceaea4599fdcf6667b7887adbcd2cada0fab5392
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-06 12:20:37 +0600

    Use of toUpperCase & toLowerCase

---
+++ case.js
@@ -0,0 +1,9 @@
+//____________Use of toUpperCase & toLowerCase______________
+
+const university = "World University of Bangladesh";
+console.log(university);
+console.log(university.toLowerCase());
+console.log(university.toUpperCase());
+
+
+
