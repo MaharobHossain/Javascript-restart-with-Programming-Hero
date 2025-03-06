e29b1f414bc3a4e3e804eb1afdd0f20164a878fe
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-06 21:30:12 +0600

    Object use in js

---
+++ object.js
@@ -0,0 +1,10 @@
+       // Object use in js
+
+    const bottle = {
+        brand: 'Apple',
+        price: 50,
+        isWater: true,
+        color: "White",
+        isClear: false
+    }
+    console.log(bottle);
