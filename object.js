9f179f7665acee1f94b0de8285b6e6df3c7d4568
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-07 07:34:28 +0600

    dot. use and [] use kore value add and change kora

--- object.js
+++ object.js
@@ -5,6 +5,21 @@
         price: 50,
         isWater: true,
         color: "White",
-        isClear: false
+        isClear: false,
+        'premium juice': ["orange", 'mango', "apple"]
     }
+
+    // console.log(bottle);
+    console.log(bottle.color);   //dot use kore kono value ber kora
+    console.log(bottle['isClear']);  // []use kore value ber kora
+    console.log(bottle['premium juice']); //proparty use kore value ber kora
+
+    //----------new value add kora---------
+    bottle.brand = 'Samsung'; // dot ar pore value change kora hoice
+    bottle.price = 70;
+    bottle.color = 'blue';
+    bottle["premium juice"] = ["orangePP", 'mangoPP', "applePP"]; // [] use kore valu change kora
     console.log(bottle);
+
+
+
