daf28f5f8936b24f4a10dc3e079300715712f999
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-08 21:17:25 +0600

    Normally For of loop use

---
+++ looping.js
@@ -0,0 +1,18 @@
+
+/**
+ * Looping in js---->
+ * For loop
+ * while loop
+ * Do while loop
+ * for in loop--->Object ke loop korar jonno use kora hoi
+ * for of loop--->Array  ke loop korar jonno use hoi
+ * 
+ */
+
+
+const friends = [ "Lalu", "Kalu", "Calu", "Balu", "Dalu", "Malu"];
+
+// Normally For of loop use
+for(const friend of friends){
+    console.log(friend);
+}
