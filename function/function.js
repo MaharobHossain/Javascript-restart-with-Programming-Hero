32bc4e919883eb80e5a0aafe94d09ec27d8c8a4e
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-09 06:49:06 +0600

    function structure and use

---
+++ function/function.js
@@ -0,0 +1,16 @@
+// -------------Function------------>
+
+//   function structure
+
+/**  function functionName(){
+ * 
+   }
+   functionName();
+*/
+
+function lightJala(){
+    console.log('uthe dara than bame side a jaa'.toUpperCase());
+    console.log('tar-pore, light ar switch a click kor'.toUpperCase());
+}
+
+lightJala();
