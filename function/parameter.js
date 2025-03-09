eb4aa7e9930e8c2305b5b2c173f59502e793a640
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-09 12:22:26 +0600

    Square or Borgo ber korar function

---
+++ function/parameter.js
@@ -0,0 +1,18 @@
+// ____________Square or Borgo ber korar function______________>
+
+/**
+ * borgo------
+ * 4*4 = 16;
+ * 5*5 = 25;
+ * 10*10 = 100;
+ * 11*11 = 121;
+ */
+
+
+
+function borgo( number ){
+    console.log('Value of the number peramitter: ', number);
+    const square = number * number;
+    console.log('Squar of the given number is: ', square);
+}
+borgo(30);
