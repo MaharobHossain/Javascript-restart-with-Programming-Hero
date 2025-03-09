fd0c501eb92c61462b202a51999212b6bd7600ac
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-09 15:41:50 +0600

    Return use in js

---
+++ function/return.js
@@ -0,0 +1,12 @@
+// Return use in js--------->
+
+function oneNumber(number){
+    const result = number * 10;
+    return result;
+}
+oneNumber(5);
+const output = oneNumber(5);
+console.log('sum is: ', output);
+
+const bigNumber = oneNumber(10);
+console.log('Jogfol hoi: ', bigNumber);
