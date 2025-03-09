8f0f991004576079042b03b9f90dcf1dd3858416
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-09 12:37:45 +0600

    Biogfoll  ber korar function

--- function/parameter.js
+++ function/parameter.js
@@ -14,8 +14,8 @@ function borgo( number ){
     const square = number * number;
     console.log('Squar of the given number is: ', square);
 }
-// borgo(30);
-// borgo(150);
+borgo(30);
+borgo(150);
 
 
 function plusNumber(num1, num2, num3){
@@ -24,3 +24,9 @@ function plusNumber(num1, num2, num3){
 }
 plusNumber(10, 21, 40);
 plusNumber(1000, 600, 200);
+
+function biogFoll( value1, value2, value3){
+    const biog = value1 + value2 - value3;
+    console.log(biog);
+}
+biogFoll(40, 20, 10);
