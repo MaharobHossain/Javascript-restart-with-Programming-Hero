f3f565f6350e6b815e459bddaea0493bf059c5ab
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-11 23:46:55 +0600

    Return use in js

--- function/params.js
+++ function/params.js
@@ -32,3 +32,21 @@ function isOdd(number){
 
 console.log(isOdd(10));
 console.log(isOdd(13));
+
+
+
+
+
+
+// Return use in js--------->
+
+function oneNumber(number){
+    const result = number * 10;
+    return result;
+}
+oneNumber(5);
+const output = oneNumber(5);
+console.log('sum is: ', output);
+
+const bigNumber = oneNumber(10);
+console.log('Jogfol hoi: ', bigNumber);
