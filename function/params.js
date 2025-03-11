c8dc0c4337377e955cc4ab7bd244b7e922d7786b
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-11 23:44:46 +0600

    negetive number find in js

--- function/params.js
+++ function/params.js
@@ -15,3 +15,20 @@ function evenSizeStaring(str){
 evenSizeStaring('Dhaka');
 evenSizeStaring("Baka");
 evenSizeStaring("Khulna");
+
+
+
+
+
+
+
+function isOdd(number){
+    if( number % 2 === 1 ){
+        return true;
+    }
+    return false;
+}
+
+
+console.log(isOdd(10));
+console.log(isOdd(13));
