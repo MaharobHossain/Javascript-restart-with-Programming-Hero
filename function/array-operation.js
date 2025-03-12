743fa150af89b9add8b1d0ac6d57499d3769a111
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-12 11:44:20 +0600

    Array operation use in js

--- function/array-operation.js
+++ function/array-operation.js
@@ -12,3 +12,20 @@ function sumNumber(numbers) {
 const num = [3, 5, 6, 3, 9];
 const sum = sumNumber(num);
 console.log(sum);
+
+
+
+
+function sumEvenNumber(numbers){
+    let sum = 0;
+    for( const number of numbers){
+        if(number %2 === 0){
+            console.log(number);
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+
+const sum = sumEvenNumber(numbers);
+console.log('Total sum is: ', sum);
