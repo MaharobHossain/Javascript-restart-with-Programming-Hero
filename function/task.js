1e586be9db3e2a023510f0a1e99f6a77b32a617a
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-13 01:41:47 +0600

    funcition modiul task 3

--- function/task.js
+++ function/task.js
@@ -6,8 +6,8 @@ function multiplyNumbers(num1, num2, num3, num4) {
     return multiple;
 }
 
-// console.log(multiplyNumbers(9, 5, 4, 7)); 
-// console.log(multiplyNumbers(8, 7, 8, 7));
+console.log(multiplyNumbers(9, 5, 4, 7)); 
+console.log(multiplyNumbers(8, 7, 8, 7));
 
 
 
@@ -15,7 +15,6 @@ function multiplyNumbers(num1, num2, num3, num4) {
 // Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
 
 // ---------This is Easy System----->
-
 // function processNumber(num) {
 //     if (num % 2 === 0) {
 //         return num / 2; // If even, divide by 2
@@ -35,3 +34,22 @@ function processNumber(num) {
 
 console.log(processNumber(15)); // Output: 30 (odd, so multiplied by 2)
 console.log(processNumber(18)); // Output: 9 (even, so divided by 2)
+
+
+
+
+//    ------------Task 3------->
+//  Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+function make_avg(arr, size) {
+    if (arr.length !== size) {
+        return "Error: Array size does not match the given size.";
+    }
+
+    let sum = arr.reduce((total, num) => total + num, 0);
+    return sum / size;
+}
+
+console.log(make_avg([10, 20, 30, 40, 50], 5)); // Output: 30
+console.log(make_avg([5, 15, 25], 3)); // Output: 15
+console.log(make_avg([5, 15, 25, 38], 3)); // Output: Error: Array size does not match the given size
