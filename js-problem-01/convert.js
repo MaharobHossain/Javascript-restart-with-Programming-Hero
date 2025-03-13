198f2e9c2568991aabc227f7090715caa3e42256
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-14 00:54:43 +0600

    Mile to Kilomiter convert

--- js-problem-01/convert.js
+++ js-problem-01/convert.js
@@ -1,8 +1,18 @@
 // inch to feet convert
-
 function inchToFeet(inch){
     const feet = inch / 12;
     return feet;
 }
 const maharobHeight = inchToFeet(70);
-console.log(maharobHeight);
+console.log('My height is: ', maharobHeight, 'feet');
+
+
+
+
+//Mile to Kilomiter convert
+function mileToKilomiter(mile){
+    const kilomiter = mile * 1.60934;
+    return kilomiter;
+}
+const islumpurToBheramara = mileToKilomiter(15);
+console.log('Islumpur to Bheramara distance is: ', islumpurToBheramara);
