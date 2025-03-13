f9cd46c568dcf5c1c09d02d0feeeaaa8c7703b66
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-14 05:50:40 +0600

    array odd item sum defiend

--- js-problem-01/odd-avarage.js
+++ js-problem-01/odd-avarage.js
@@ -1,13 +1,25 @@
 // array odd item sum defiend
 
 function oddAvarage(numbers){
+
+    const odds = [];
     for( const number of numbers ){
         if( number % 2 === 1){
-            console.log(number);
+           // console.log(number);
+            odds.push(number);
         }
     }
+    // console.log(odds);
+    let sum = 0;
+    for(const number of odds){
+        sum = sum + number;
+    }
+    const count = odds.length;
+    console.log(sum, count);
+    const avg = sum / count;
+    return avg;
 }
 
-const numbers = [45, 25, 68, 50, 42, 69, 49];
+const numbers = [45, 25, 68, 50, 42, 69, 49, 533];
 const avg = oddAvarage(numbers);
 console.log('Avarege of the odd number is: ', avg);
