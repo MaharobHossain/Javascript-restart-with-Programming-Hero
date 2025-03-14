f5a4e54215c29bf7f2ec5e1aded2e3eaafdb4679
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-14 16:25:31 +0600

    Javascriptdifferent  math use

--- js-problem-01/math.js
+++ js-problem-01/math.js
@@ -8,3 +8,17 @@ console.log('Minimum number is:', min);
 
 const max = Math.max(342, 556, 67, 93, 26, 12, 8, 1000);
 console.log('Maximum number is:', max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(-88), Math.abs(44-99));
+console.log(Math.round(2.30), Math.round(2.85)); 
+
+console.log(Math.floor(2.99)); // jotoi hok na kno point ar pore niche namai niaa asbe.
+console.log(Math.ceil(2.111)); // point ar porei jotoi thakuk na kno point ar pore barai dibe.
+
+console.log(Math.random()); // Randomlly akek bar alada alada value dei.
+console.log(Math.random()* 10);
+
+const rand = Math.round(Math.random()*8);
+console.log(rand);
