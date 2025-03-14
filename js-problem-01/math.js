3e7307b7fdf8b7dcc7cd9419cf945046b0c6fd7f
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-14 15:58:37 +0600

    maximum ba boro sonhka ber kora

--- js-problem-01/math.js
+++ js-problem-01/math.js
@@ -1,4 +1,10 @@
 // minimum ba chotto sonhka ber kora
 
 const min = Math.min(342, 556, 67, 93, 26, 12, 8);
-console.log(min);
+console.log('Minimum number is:', min);
+
+
+// maximum ba boro sonhka ber kora
+
+const max = Math.max(342, 556, 67, 93, 26, 12, 8, 1000);
+console.log('Maximum number is:', max);
