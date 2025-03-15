8cf41b436abb08ec8824a6cc631c3a5e69c9d24c
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-15 17:23:15 +0600

    3 ta sonkhar moddhe theke boro sonkha ber kora if-else use kore

---
+++ js-problem-02/max3.js
@@ -0,0 +1,15 @@
+// 3 ta sonkhar moddhe theke boro sonkha ber kora if-else use kore
+
+let bati = 90;
+let kiminal = 95;
+let kasem = 99;
+
+if( bati > kiminal && bati > kasem ){
+    console.log("Bati is a good student");
+}
+else if( kiminal > bati && kiminal > kasem ){
+    console.log("Kiminal is a good student");
+}
+else{
+    console.log("Kasem is a good student");
+}
