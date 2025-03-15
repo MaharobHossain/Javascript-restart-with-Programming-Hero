2110b6db57c23cf0fef24b4c58e3095cfeea7c3c
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-15 15:57:54 +0600

    find the maximum number use if else

---
+++ js-problem-02/max.js
@@ -0,0 +1,12 @@
+// find the maximum number use if else
+
+let daimul = 88;
+let kasem = 79;
+
+if(daimul > kasem){
+    console.log("Daimul win the game");
+}
+else{
+    console.log("Kasem win the game");
+}
+
