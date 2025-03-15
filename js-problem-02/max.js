5ebd3e0659951b8cb4ed724ad342ec004c28d528
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-15 17:15:15 +0600

    find the maximum number use a function

--- js-problem-02/max.js
+++ js-problem-02/max.js
@@ -10,3 +10,23 @@ else{
     console.log("Kasem win the game");
 }
 
+
+
+
+// find the maximum number use a function
+
+function getMax(num1, num2) {
+    if(num1 > num2){
+        return num1;
+    }
+    else{
+        return num2;
+    }
+}
+
+const max1 = getMax(85, 69);
+const max2 = getMax(63, 98);
+console.log("Max of the tow is:", max1, max2);
+const ultimateMax = getMax(max1, max2);
+console.log("Max of the tow mort is:", ultimateMax);
+
