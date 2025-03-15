9815223c5906c3b8633a27837378f2e6e345b95a
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-15 17:30:25 +0600

    3 ta sonkhar moddhe theke boro sonkha ber kora function use kore

--- js-problem-02/max3.js
+++ js-problem-02/max3.js
@@ -13,3 +13,23 @@ else if( kiminal > bati && kiminal > kasem ){
 else{
     console.log("Kasem is a good student");
 }
+
+
+
+
+
+// 3 ta sonkhar moddhe theke boro sonkha ber kora function use kore
+
+function getMaxOfThree( num1, num2, num3){
+    if( num1 > num2 && num1 > num3 ){
+        return num1;
+    }
+    else if( num2 > num1 && num2 > num3 ){
+        return num2;
+    }
+    else{
+        return num3;
+    }
+}
+const max1 = getMaxOfThree(99, 57, 98);
+console.log(max1);
