fc4f50e3ade240b2cfaa3b1f7ce7bc754fb4f3a3
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-15 06:18:13 +0600

    Task 2 complete

--- js-problem-01/task.js
+++ js-problem-01/task.js
@@ -1,3 +1,6 @@
+//---------Task-1:
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
 function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
@@ -5,3 +8,37 @@ function celsiusToFahrenheit(celsius) {
 console.log(celsiusToFahrenheit(0));   // Output: 32
 console.log(celsiusToFahrenheit(250));  // Output: 77
 console.log(celsiusToFahrenheit(-10)); // Output: 14
+
+
+
+
+
+//---------Task-2:
+
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+// sample-input: numbers = [5,6,11,12,98, 5]
+
+// find: 5
+
+// output: 2
+
+
+// sample-input:
+
+// numbers = [5,6,11,12,98, 5]
+
+// find: 25
+
+// output: 0
+
+
+function countOccurrences(arr, target) {
+    return arr.filter(num => num === target).length;
+}
+
+// Sample inputs
+console.log(countOccurrences([5, 6, 11, 12, 98, 5], 5));  // Output: 2
+console.log(countOccurrences([5, 6, 11, 12, 98, 5], 25)); // Output: 0
+
+
