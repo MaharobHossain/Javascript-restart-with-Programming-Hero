7737e066eec5bc7f168eb44023fa10c30d610e84
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-16 16:53:23 +0600

    All phone avrage peice find task 4

--- js-problem-02/task.js
+++ js-problem-02/task.js
@@ -84,3 +84,51 @@ function calculateElectronicsBudget(laptopQuantity, tabletQuantity, mobileQuanti
  let totalPrice = calculateElectronicsBudget(3, 5, 7);
  console.log('Total price is: ', totalPrice);
  // console.log(calculateElectronicsBudget(2, 1, 3)); // Output: 145000
+
+
+
+
+
+
+ /**
+  * Task-4:
+You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
+
+Input
+
+
+    const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
+  */
+
+    const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
+
+    function findAveragePhonePrice(phones) {
+        let totalPrice = 0;
+    
+        for (let phone of phones) {
+            totalPrice += phone.price; // Add up all phone prices
+        }
+    
+        const averagePrice = totalPrice / phones.length; // Calculate the average
+        return averagePrice;
+    }
+    
+
+    let allPhonePrice = findAveragePhonePrice(phones);
+    console.log("All phone avrage price is: ", allPhonePrice);
+    // console.log(findAveragePhonePrice(phones)); // Output: 58333.33
+    
