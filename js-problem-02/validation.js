d1f81e7c592562c71f2e5e79755fff35f97a31c3
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-16 15:34:20 +0600

    Valid string provider alert (Full Name)

--- js-problem-02/validation.js
+++ js-problem-02/validation.js
@@ -11,4 +11,24 @@ function multiply(num1, num2){
 }
 
 const result = multiply(7, 'ten');
-console.log(result);
+//console.log(result);
+
+
+
+// Valid string provider alert (Full Name)
+
+function fullName( firstName, lastName){
+
+    //  console.log(typeof firstName, typeof lastName);
+    if( typeof firstName !== 'string' ){
+        return 'First name shoud be a string';
+    }
+    else if(typeof lastName !== 'string'){
+        return 'Last name shoud be a string';
+    }
+    let full = firstName + '  ' + lastName;
+    return full;
+}
+
+let full = fullName(4, 'Ali');
+console.log(full);
