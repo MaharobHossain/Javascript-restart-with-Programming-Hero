a93a2446c0303897c9834f9cfe8bd363e0eca038
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-16 07:29:53 +0600

    Create a calculator with js

--- js-problem-02/calculator.js
+++ js-problem-02/calculator.js
@@ -16,3 +16,28 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
+function calculator(a, b, operation) {
+    
+    if( operation === 'add'){
+        const result = add(a, b);
+        return result;
+    }
+    else if(operation === 'subtract'){
+        const result = subtract(a, b);
+        return result;
+    }
+    else if(operation === 'multiply'){
+        const result = multiply(a, b);
+        return result;
+    }
+    else if(operation === 'divide'){
+        const result = divide(a, b);
+        return result;
+    }
+    else{
+        return "Only 'add', 'subtract', 'multiply', 'divide' operation is allowed. ";
+    }
+}
+
+let result = calculator(8, 7, 'multiply');
+console.log("Your result is:", result);
