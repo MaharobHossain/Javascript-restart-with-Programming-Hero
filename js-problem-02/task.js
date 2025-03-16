b8ad8806a589e7ba1f68ed657190a229fb9bc8bb
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-16 16:41:16 +0600

    Task  2  - calculation all product price

--- js-problem-02/task.js
+++ js-problem-02/task.js
@@ -4,13 +4,13 @@ Find the lowest number in the array below.
 const heights2 = [167, 190, 120, 165, 137];
 */
 
-// const heights2 = [167, 190, 120, 165, 137];
+const heights1 = [167, 190, 120, 165, 137];
 
-// function findLowestNumber(arr) {
-//     return Math.min(...arr);
-// }
+function findLowestNumber(arr) {
+    return Math.min(...arr);
+}
 
-// console.log(findLowestNumber(heights2)); 
+console.log(findLowestNumber(heights1)); 
 
 const heights2 = [167, 190, 120, 165, 137];
 
@@ -26,3 +26,32 @@ function getMin(numbers){
 const cheap = getMin(heights2);
 console.log("The cheapest price is:", cheap);
 
+
+
+
+
+/**
+ * Task-3:
+Your task is to calculate the total budget required to buy electronics:
+
+    laptop = 35000 tk
+    tablet = 15000 tk
+    mobile = 20000 tk
+Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
+
+ */
+
+
+function calculateElectronicsBudget(laptopQuantity, tabletQuantity, mobileQuantity) {
+    const laptopPrice = 35000;
+    const tabletPrice = 15000;
+    const mobilePrice = 20000;
+
+    const totalBudget = (laptopQuantity * laptopPrice) + (tabletQuantity * tabletPrice) + (mobileQuantity * mobilePrice);
+    
+    return totalBudget;
+}
+
+ let totalPrice = calculateElectronicsBudget(3, 5, 7);
+ console.log('Total price is: ', totalPrice);
+ // console.log(calculateElectronicsBudget(2, 1, 3)); // Output: 145000
